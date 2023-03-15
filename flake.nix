7fefde5d7b500852ad83013d69b004a32d4aab26
Alexander Sauceda <alexander@alexandersauceda.dev>
2023-03-14 21:44:53 -0600

    Adding Nix and direnv dev shell

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Dev environment for Hugo static website generator";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      # TODO: add shell hook to start tmux session and run Hugo live reload server
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = with pkgs; [
+            go_1_19 # Go 1.19
+            gotools # Go tools like goimports, godoc, and others
+            hugo # Static website generator
+          ];
+        };
+      });
+    };
+}
